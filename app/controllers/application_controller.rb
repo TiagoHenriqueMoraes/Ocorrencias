4f0ca49e2fd7e3bbaf6539e2e404a7be604c358c
TiagoMoraes <tiago_h_moraes@hotmail.com>
2018-10-12 20:31:57 -0300

    Created controller and models for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from ActiveRecord::RecordNotFound,
+                Pundit::NotAuthorizedError,
+                ActionController::RoutingError, with: :user_not_authorized
+  end
+
+  before_action :authenticate_user!
+
+  private
+  
+  def user_not_authorized
+    render template: "errors/404", status: :not_found
+  end
 end
