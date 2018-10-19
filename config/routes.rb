b23a46fcafadef096384f88a4787e30238767859
TiagoMoraes <tiago_h_moraes@hotmail.com>
2018-10-18 21:00:34 -0300

    Working on views

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :guidelines, expect: :show
   resources :teams, expect: :show
   resources :occurrences
-  resources :supervisors
   resources :turns, expect: :show
   resources :groups, expect: :show
   resources :employees
